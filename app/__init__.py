20e8c21b88bac8ca700604debb613f51f3e6d1d5
ywalakamar <barxwells@gmail.com>
2020-01-20 22:09:01 +0300

    chore(blueprints) create blueprint and views - create register endpoint - create login endpoint - created custom  decorators - close #5

--- app/__init__.py
+++ app/__init__.py
@@ -28,5 +28,7 @@ def create_app(test_config=None):
         return "Hello, world!"
 
     from . import db
-    db.init_app(app)   
+    db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
     return app
